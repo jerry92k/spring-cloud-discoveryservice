7e7c3b0d8489e551825ba0e30a7af1c4d90039ed
jerry92k <jens92.k@gmail.com>
2021-06-21 15:56:30 +0900

    init spring-cloud - eurekaserver

---
+++ src/test/java/com/example/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
