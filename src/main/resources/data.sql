c7174721e87cca99a78a9092b490ac5e37c5096f
jerry92k <jens92.k@gmail.com>
2021-06-30 22:18:40 +0900

    kafka connect

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into catalog(product_id, product_name, stock, unit_price)
-    values('catalog-01','berlin',100,1500);
+    values('catalog-001','berlin',100,1500);
 
 insert into catalog(product_id, product_name, stock, unit_price)
-values('catalog-02','kelln',220,1300);
+values('catalog-002','kelln',220,1300);
 
 insert into catalog(product_id, product_name, stock, unit_price)
-values('catalog-03','frankfrut',500,1200);
+values('catalog-003','frankfrut',500,1200);
