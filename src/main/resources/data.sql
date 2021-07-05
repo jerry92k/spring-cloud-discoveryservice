a88a714e578ea83753288c1129bf45b01fa8d791
jerry92k <jens92.k@gmail.com>
2021-07-05 11:45:27 +0900

    docker connect

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into catalog(product_id, product_name, stock, unit_price)
-    values('catalog-001','berlin',100,1500);
-
-insert into catalog(product_id, product_name, stock, unit_price)
-values('catalog-002','kelln',220,1300);
-
-insert into catalog(product_id, product_name, stock, unit_price)
-values('catalog-003','frankfrut',500,1200);
